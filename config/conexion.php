e300a1815e866e2c8b0c97ec2353f7eccc6b07b3
Nsanchez29 <60805500+Nsanchez29@users.noreply.github.com>
2020-10-12 22:13:43 -0600

    Conexion
    Conexion a base de Datos

---
+++ config/conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+require_once "global.php";
+
+
+$conexion = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+
+if(mysqli_connect_errno()){
+
+
+        echo "No se ha podido conectar a la base de datos";
+
+        exit();
+
+}
+
+
+if(!function_exists('ejecutarConsulta')){
+
+        echo "Conexion a base de datos exitosa";
+
+}
+
+ ?>
+
---
+++ config/global.php
@@ -0,0 +1,16 @@
+<?php
+
+
+define("DB_HOST", "69.46.6.238:3306");
+
+define("DB_NAME", "tcsogt_restaurante");
+
+define("DB_USERNAME", "tcsogt_nsanchez"); 
+
+define("DB_PASSWORD", "tcsogt123*");
+
+define("DB_ECONDE", "utf8");
+
+define("PRO_NOMBRE", "RESTAURANTE 2020");
+
+ ?>
