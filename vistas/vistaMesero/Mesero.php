0743ee9a940e455316470ad33525183379b12a3b
Mynor98 <60805444+Mynor98@users.noreply.github.com>
2020-10-13 21:17:04 -0600

    cambios en vista mesero

---
+++ vistas/vistaMesero/Mesero.php
@@ -0,0 +1,62 @@
+<?php
+
+session_start();
+/*
+$usuario = $_SESSION['usuario'];
+
+if(!isset($usuario)){
+  header("location: ../vistas/login.php");
+}else{
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Proyecto Restaurante</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+      crossorigin="anonymous"
+    />
+    <!-- JS, Popper.js, and jQuery -->
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+      integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+      integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="../../css/styles.css" />
+  </head>
+  <body>
+    
+    <?php include("../navbar/navbarMesero.php");?>
+
+    <br><br>
+
+    <div style="margin: auto;" class="col-md-11 card shadow-inset">
+        <div class="card-body col-md-12">
+            <div class="item">
+                <h3>Mesas</h3>
+                <span class="spacer"></span>
+                <button type="button" class="btn btn-primary">Agregar</button>
+            </div>
+            <hr>
+
+        </div>
+    </div>
+
+  </body>
+</html>
+<?php /*}*/ ?>
