97922435506f143d8c29ea4bd843df02779de8bd
Nsanchez29 <60805500+Nsanchez29@users.noreply.github.com>
2020-10-20 21:25:25 -0600

    cambios en asignar mesero

--- modelos/asignarMeseros.php
+++ modelos/asignarMeseros.php
@@ -11,18 +11,17 @@ $Mesero = $_POST['mesero'];
 
 require_once "../config/conexion.php";
 
-$consulta = "SELECT * FROM `ordenes` WHERE fecha > CURRENT_DATE()";
+$consulta = "SELECT COUNT(*) as cantidad FROM ordenes where fecha > CURRENT_DATE()";
 $q = mysqli_query($conexion, $consulta);
-$result = mysqli_num_rows($q);
+$fila=mysqli_fetch_array($q);
 
-while ($fila=mysqli_fetch_array($q)){
+$cantidad = $fila['cantidad'];
 
 
-if ($result!=0) {
+//echo $cantidad;
 	
-	
 
-	$Orden = $fila['numero']+1; 
+	$Orden = $cantidad+1; 
 	//echo $Orden;
 	//echo $idMesa;
 	//echo $NMesa;
@@ -49,44 +48,13 @@ if ($result!=0) {
 		}
 		
 	}else{
-		echo '<script> alert("Error al Asignar la Mesa 1");
-		location.href = "../vistas/vistaAnfitrion/Anfitrion.php"; 
-		</script>';
-	}
-	
-
-
-
-}else{
-	
-	//echo "No hay";
-	$asignacionMesa = "INSERT INTO ordenes (numero,idMesa,total,idUsuario) VALUES ('1','$idMesa','0','$Mesero')";
-	$estadoMesa = "UPDATE mesas SET estado = 2 where id = $idMesa";
-
-	$Query = mysqli_query($conexion, $asignacionMesa);
-	$Query2 = mysqli_query($conexion, $estadoMesa);
-
-	if ($Query) {
-
-		if ($Query2) {
-			echo '<script> alert("Mesa Asignada Exitosamente");
-		location.href = "..vistas/vistaAnfitrion/Anfitrion.php"; 
-		</script>';
-		}else{
-			echo '<script> alert("Error al Asignar la Mesa");
-		location.href = "../vistas/vistaAnfitrion/Anfitrion.php"; 
-		</script>';
-		}
-
-		
-	}else{
 		echo '<script> alert("Error al Asignar la Mesa");
 		location.href = "../vistas/vistaAnfitrion/Anfitrion.php"; 
 		</script>';
 	}
+	
 
-}
 
-}
+
 
 ?>
